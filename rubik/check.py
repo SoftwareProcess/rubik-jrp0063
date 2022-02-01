ac6c35f2f4b92699715a154f1ffe39322c99490d
Jonathon Porco <jonathon.porco@gmail.com>
2022-01-31 19:37:23 -0600

    a9psrgoehbpoiefubhiufbwe done??????????????????

--- rubik/check.py
+++ rubik/check.py
@@ -1,10 +1,41 @@
 import rubik.cube as rubik
+import collections
 
 def _check(parms):
+    
     result={}
-    encodedCube = parms.get('cube',None)       
-    if(encodedCube == None):
-        result['status'] = 'error: xxx'
+    encodedCube = parms.get('cube', None)
+    
+    # encodedCube is empty    
+    if encodedCube == None:
+        result['status'] = 'error: cube missing'
+        
+    # encodedCube is a string
+    elif not isinstance(encodedCube, str):
+        result['status'] = 'error: not a string'
+        
+    # encodedCube is alphanumeric
+    elif encodedCube.isalnum() == False:
+        result['status'] = 'error: invalid characters'
+    
+    # encodedCube is 54 characters
+    elif len(encodedCube) != 54:
+        result['status'] = 'error: invalid size'
+        
+    # encodedCube contains 6 colors
+    elif len(collections.Counter(encodedCube)) != 6:
+        result['status'] = 'error: number of colors'
+        
+    # encodedCube color centers are at indices 4, 13, 22, 31, 40, and 49
+    elif len(set(encodedCube[i] for i in [4, 13, 22, 31, 40, 49])) != 6:
+        result['status'] = 'error: invalid centers'
+    
+    # encodedCube contains 9 of each color
+    elif str(collections.Counter(encodedCube).values()) != 'dict_values([9, 9, 9, 9, 9, 9])':
+        result['status'] = 'error: amount of each color'
+
+    # valid
     else:
         result['status'] = 'ok'
+        
     return result
--- rubik/test/checkTest.py
+++ rubik/test/checkTest.py
@@ -3,11 +3,75 @@ import rubik.check as check
 
 class CheckTest(TestCase):
         
-    def test_check_010_ShouldReturnOkOnSolvedCube(self):
+    def test_check_01_Ok_SolvedCube(self):
         parm = {'op':'check',
                 'cube':'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
         result = check._check(parm)
         self.assertIn('status', result)
-        status = result.get('status', None)
+        status = result.get('status')
         self.assertEqual(status, 'ok')
+        print("CT 01 PASSED - Ok_SolvedCube")
 
+    def test_check_02_Error_CubeMissing(self):
+        parm = {'op':'check',
+                'cube': None}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertEqual(status, 'error: cube missing')
+        print("CT 02 PASSED - Error_CubeMissing")
+        
+    def test_check_03_Error_NotAString(self):
+        parm = {'op':'check',
+                'cube': 123456789}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertEqual(status, 'error: not a string')
+        print("CT 03 PASSED - Error_NotAString")
+        
+    def test_check_04_Error_InvalidCharacters(self):
+        parm = {'op':'check',
+                'cube': '!#%&?'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertEqual(status, 'error: invalid characters')
+        print("CT 04 PASSED - Error_InvalidCharacters")
+
+    def test_check_05_Error_InvalidSize(self):
+        parm = {'op':'check',
+                'cube':'0123456789'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertEqual(status, 'error: invalid size')
+        print("CT 05 PASSED - Error_InvalidSize")
+        
+    def test_check_06_Error_NumberOfColors(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbbrrrrrrrrggggggggooooooooyyyyyyyywwwwwwwwzzzzzz'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertEqual(status, 'error: number of colors')
+        print("CT 06 PASSED - Error_NumberOfColors")
+        
+    def test_check_07_Error_InvalidCenters(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbbbrrrrgrrrrggggoggggooooyooooyyyywyyyywwwwbwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertEqual(status, 'error: invalid centers')
+        print("CT 07 PASSED - Error_InvalidCenters")
+        
+    def test_check_08_Error_AmountOfEachColor(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbbbbbbbrbbbbgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertEqual(status, 'error: amount of each color')
+        print("CT 08 PASSED - Error_AmountOfEachColor")
+        
