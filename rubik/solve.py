f2687c79b89e93014dbdf94d12ca8e27d82a9af5
Jonathon Porco <jonathon.porco@gmail.com>
2022-03-01 14:16:08 -0600

    Yellow Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -1,7 +1,5 @@
-import rubik.cube as rubik
 import rubik.check as check
 
 def _solve(parms):
-    result = {}
     if (cube_result := check(parms)).get('status') != 'ok':
         return cube_result
