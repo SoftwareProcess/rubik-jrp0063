ee142704e58dca9c331310d5d9c56f7817791e83
Jonathon Porco <jonathon.porco@gmail.com>
2022-03-01 15:37:50 -0600

    Yellow Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -1,16 +1,36 @@
 from rubik.check import _check
+from rubik.cube import Cube
+
+# NOTES:
+#    use all() to check if a string contains certain characters
 
 ROTATION_CHARACTERS = set('FfRrBbLlUuDd')
 
 def _solve(parms):
     
-    if (cube_result := _check(parms)).get('status') != 'ok':
-        return cube_result
+    #if (cube_result := _check(parms)).get('status') != 'ok':
+        #return cube_result
     
     result = {}
-    rotate = parms.get('rotate')
-    if rotate is None:
-        rotate = 'F'
+    rotations = parms.get('rotate')
+    # rotate defaults to 'F' if missing
+    if rotations is None:
+        rotations = 'F'
     
-    if not isinstance(rotate, str):
+    #
+    if not isinstance(rotations, str):
         result['status'] = ''
+    #
+    elif not rotations.isalpha():
+        result['status'] = ''
+    #
+    elif not all(rotations in ROTATION_CHARACTERS for rotation in rotations):
+        result['status'] = ''
+    else:
+        cube = Cube(parms.get('cube'))
+        for rotation in rotations:
+            cube.rotate(rotation)
+        result['status'] = 'ok'
+        result['cube'] = str(cube)
+    return result
+            
--- rubik/test/solveTest.py
+++ rubik/test/solveTest.py
@@ -4,39 +4,83 @@ import rubik.solve as solve
 class SolveTest(unittest.TestCase):
     
     def test_solve_01_emptycube(self):
-        parms = {'op': 'solve'}
-        result = solve._solve(parms)
+        parm = {'op': 'solve'}
+        result = solve._solve(parm)
         self.assertIn('status', result)
         status = result.get('status')
         self.assertNotEqual('ok', status)
     
-    @unittest.skip('cube model incomplete')
+    @unittest.skip('error messages incomplete')
     def test_solve_02_badcube(self):
-        pass
+        parm = {'op': 'solve',
+                'cube': 9}
+        result = solve._solve(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertNotEqual('ok', status)
     
-    @unittest.skip('cube rotate incomplete')
+    @unittest.skip('error messages incomplete')
     def test_solve_03_rotate_notstring(self):
-        pass
+        parm = {'op': 'solve',
+                'rotate': 9,
+                'cube': 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'}
+        result = solve._solve(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertNotEqual('ok', status)
     
-    @unittest.skip('cube rotate incomplete')
+    @unittest.skip('error messages incomplete')
     def test_solve_04_rotate_notletter(self):
-        pass
+        parm = {'op': 'solve',
+                'rotate': '9',
+                'cube': 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'}
+        result = solve._solve(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertNotEqual('ok', status)
     
     @unittest.skip('cube rotate incomplete')
     def test_solve_05_rotate_empty(self):
-        pass
+        parm = {'op': 'solve',
+                'rotate': '',
+                'cube': 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'}
+        result = solve._solve(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertEqual(status, 'ok')
+        cube = result.get('cube')
+        self.assertEqual(cube, 'gggggggggwrrwrrwrrbbbbbbbbbooyooyooywwwwwwooorrryyyyyy')
     
     @unittest.skip('cube rotate incomplete')
     def test_solve_06_rotate_missing(self):
-        parms = {'op':'solve',
-                'rotate':'',
-                'cube':'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'}
+        parm = {'op': 'solve',
+                'cube': 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'}
+        result = solve._solve(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertEqual(status, 'ok')
+        cube = result.get('cube')
+        self.assertEqual(cube, 'gggggggggwrrwrrwrrbbbbbbbbbooyooyooywwwwwwooorrryyyyyy')
     
-    @unittest.skip('cube rotate incomplete')
+    @unittest.skip('error messages incomplete')
     def test_solve_07_rotate_invalidchars(self):
-        pass
+        parm = {'op': 'solve',
+                'rotate': 'x',
+                'cube': 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'}
+        result = solve._solve(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertNotEqual('ok', status)
     
     @unittest.skip('cube rotate incomplete')
     def test_solve_08_rotate_valid(self):
-        pass
+        parm = {'op': 'solve',
+                'rotate': 'F',
+                'cube': 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'}
+        result = solve._solve(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertEqual(status, 'ok')
+        cube = result.get('cube')
+        self.assertEqual(cube, 'gggggggggwrrwrrwrrbbbbbbbbbooyooyooywwwwwwooorrryyyyyy')
         
