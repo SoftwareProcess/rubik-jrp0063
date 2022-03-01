e243668ab52fec2671c77bcea3889fc4d8bd032f
Jonathon Porco <jonathon.porco@gmail.com>
2022-03-01 14:55:00 -0600

    Yellow Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -1,7 +1,13 @@
 from rubik.check import _check
 
+ROTATION_CHARACTERS = set('FfRrBbLlUuDd')
+
 def _solve(parms):
-    result = {}
     
-    if (cube_result := _check(parms)).get('status') != 'ok':
-        return cube_result
+    result = {}
+    rotate = parms.get('rotate')
+    if rotate is None:
+        rotate = 'F'
+    
+    if not isinstance(rotate, str):
+        result['status'] = ''
