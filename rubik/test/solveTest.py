96ac5df574620ded4fd378f3d475b65b88918a7b
Jonathon Porco <jonathon.porco@gmail.com>
2022-03-01 14:41:15 -0600

    Green Light |

--- rubik/test/solveTest.py
+++ rubik/test/solveTest.py
@@ -9,3 +9,27 @@ class SolveTest(unittest.TestCase):
         self.assertIn('status', result)
         status = result.get('status')
         self.assertNotEqual('ok', status)
+    
+    @unittest.skip
+    def test_solve_02_badcube(self):
+        pass
+    
+    
+    def test_solve_03_rotate_notstring(self):
+        pass
+    
+    def test_solve_04_rotate_notletter(self):
+        pass
+    
+    def test_solve_05_rotate_empty(self):
+        pass
+    
+    def test_solve_06_rotate_missing(self):
+        pass
+    
+    def test_solve_07_rotate_invalidchars(self):
+        pass
+    
+    def test_solve_08_rotate_valid(self):
+        pass
+        
