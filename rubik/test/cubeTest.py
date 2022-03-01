7d99e2720aff518be88362e5e683dd83cfc1c9b9
Jonathon Porco <jonathon.porco@gmail.com>
2022-03-01 11:20:35 -0600

    Yellow Light |

--- rubik/test/cubeTest.py
+++ rubik/test/cubeTest.py
@@ -5,5 +5,5 @@ class CubeTest(unittest.TestCase):
 
     def test_cube_01_cuberead(self):
         cube_str = 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
-        cube = Cube(cube_str)
+        cube = cube(cube_str)
         self.assertEqual(str(cube), cube_str)
