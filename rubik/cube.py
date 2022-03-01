7c71e0d7e84f5986ce6a2b13fd7b112139061227
Jonathon Porco <jonathon.porco@gmail.com>
2022-03-01 11:53:27 -0600

    Yellow Light |

--- rubik/cube.py
+++ rubik/cube.py
@@ -1,10 +1,13 @@
 # sides are read in this order from sequence
 SIDES = ('front', 'right', 'back', 'left', 'up', 'down')
 
+
+
 class Cube:
     
     
     def __init__(self, cube_str: str):
+        self.cube_str = cube_str
         self.faces = {
             face: [
                 [cube_str[offset + 0], cube_str[offset + 1], cube_str[offset + 2]],
@@ -14,5 +17,12 @@ class Cube:
             for face, offset in zip(SIDES, (0, 9, 18, 27, 36, 45))
             }
         
+        
     def __getitem__(self, item: int) -> str:
         return self.faces[SIDES[item // 9]][item % 9 // 3][item % 3]
+    
+    def show(self):
+        print(self.cube_str)
+        
+testcube = Cube('gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy')
+testcube.show()
