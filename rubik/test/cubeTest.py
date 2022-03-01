07c50104eb6aead835f4be27ba94aa33c0ab02e2
Jonathon Porco <jonathon.porco@gmail.com>
2022-03-01 15:57:04 -0600

    Yellow Light |

--- rubik/test/cubeTest.py
+++ rubik/test/cubeTest.py
@@ -5,9 +5,19 @@ class CubeTest(unittest.TestCase):
 
     def test_cube_01_cuberead(self):
         cube_str = 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
-        test_cube = Cube(cube_str)
-        self.assertEqual(cube_str, str(test_cube))
+        cube = Cube(cube_str)
+        self.assertEqual(cube_str, str(cube))
         
     @unittest.skip('cube rotate incomplete')
     def test_cube_02_cuberotate_F(self):
-        pass
+        cube_str = 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
+        cube = Cube(cube_str)
+        cube.rotate('F')
+        self.assertEqual(str(cube), 'gggggggggwrrwrrwrrbbbbbbbbbooyooyooywwwwwwooorrryyyyyy')
+        
+    @unittest.skip('cube rotate incomplete')
+    def test_cube_03_cuberotate_f(self):
+        cube_str = 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
+        cube = Cube(cube_str)
+        cube.rotate('f')
+        self.assertEqual(str(cube), 'gggggggggyrryrryrrbbbbbbbbboowoowoowwwwwwwrrroooyyyyyy')
