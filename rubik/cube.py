644621bff809253f5c06a157e3c01fea3f0ec238
Jonathon Porco <jonathon.porco@gmail.com>
2022-03-02 13:43:00 -0600

    Yellow Light |

--- rubik/cube.py
+++ rubik/cube.py
@@ -75,15 +75,15 @@ class Cube:
                 self[d] = temp
     
     def __str__(self):
-        result = "".join(
-            f"{f[0][0]}{f[0][1]}{f[0][2]}{f[1][0]}{f[1][1]}{f[1][2]}{f[2][0]}{f[2][1]}{f[2][2]}"
-            for f in (self.faces[face] for face in FACE_NAMES)
+        result = "\n".join(
+            face + "\n" + "\n".join(", ".join(row) for row in self.faces[face]) + "\n" for face in FACE_NAMES
         )
         return result
     
     def __repr__(self) -> str:
-        result = "\n".join(
-            face + "\n" + "\n".join(", ".join(row) for row in self.faces[face]) + "\n" for face in FACE_NAMES
+        result = "".join(
+            f"{f[0][0]}{f[0][1]}{f[0][2]}{f[1][0]}{f[1][1]}{f[1][2]}{f[2][0]}{f[2][1]}{f[2][2]}"
+            for f in (self.faces[face] for face in FACE_NAMES)
         )
         return result
         
