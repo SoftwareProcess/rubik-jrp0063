23bddd90fd8b53806e8aae92f1621438bfc11198
Jonathon Porco <jonathon.porco@gmail.com>
2022-03-01 21:33:03 -0600

    Yellow Light |

--- rubik/test/solveTest.py
+++ rubik/test/solveTest.py
@@ -1,11 +1,11 @@
 import unittest
-from rubik.solve import solve
+import rubik.solve as solve
 
 class SolveTest(unittest.TestCase):
     
     def test_solve_01_emptycube(self):
         parm = {'op': 'solve'}
-        result = solve.solve(parm)
+        result = solve._solve(parm)
         self.assertIn('status', result)
         status = result.get('status')
         self.assertNotEqual('ok', status)
