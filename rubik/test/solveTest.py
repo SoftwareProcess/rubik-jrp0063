5374fca3dfb14a5aedb528c0777d53eada224992
Jonathon Porco <jonathon.porco@gmail.com>
2022-03-03 21:07:13 -0600

    Yellow Light |

--- rubik/test/solveTest.py
+++ rubik/test/solveTest.py
@@ -63,7 +63,7 @@ class SolveTest(unittest.TestCase):
         
     def test_solve_09_rotate_valid_all(self):
         parm = {'op': 'solve',
-                'rotate': 'F',
+                'rotate': 'FRBLUDfrblud',
                 'cube': 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'}
         result = solve._solve(parm)
         self.assertIn('status', result)
