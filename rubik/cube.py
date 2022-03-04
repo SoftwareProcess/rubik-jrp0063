3887b37d9e9e74b2a648eea511d59fa763fdd751
Jonathon Porco <jonathon.porco@gmail.com>
2022-03-04 01:18:55 -0600

    Yellow Light |

--- rubik/cube.py
+++ rubik/cube.py
@@ -7,17 +7,18 @@
 
 # JONATHON FIX THIS IT'S UGLY
 ORIENTATIONS = {'f': 'front', 'r': 'right', 'b': 'back', 'l': 'left', 'u': 'up', 'd': 'down'}
+OFFSETS = {'f': 0, 'r': 9, 'b': 18, 'l': 27, 'u': 36, 'd': 45}
 JUST_NAMES = list(ORIENTATIONS.values())
 JUST_ABBR = list(ORIENTATIONS.keys())
-OFFSETS = {'f': 0, 'r': 9, 'b': 18, 'l': 27, 'u': 36, 'd': 45}
 
-ADJACENTS = {   'f': ((42, 43, 44), (9, 12, 15), (47, 46, 45), (35, 32, 29)),   'r': ((44, 41, 38), (18, 21, 24), (53, 50, 47), (8, 5, 2)),
+CONNECTED = {   'f': ((42, 43, 44), (9, 12, 15), (47, 46, 45), (35, 32, 29)),   'r': ((44, 41, 38), (18, 21, 24), (53, 50, 47), (8, 5, 2)),
                 'b': ((38, 37, 36), (27, 30, 33), (51, 52, 53), (17, 14, 11)),  'l': ((36, 39, 42), (0, 3, 6), (45, 48, 51), (26, 23, 20)),
                 'u': ((20, 19, 18), (11, 10, 9), (2, 1, 0), (29, 28, 27)),      'd': ((6, 7, 8), (15, 16, 17), (24, 25, 26), (33, 34, 35))}
 
 class Cube:
+    
+    # called on instance creation
     def __init__(self, cube_str):
-        
         self.cube_build = {
             colors: [
                 [cube_str[offset + 0], cube_str[offset + 1], cube_str[offset + 2]],
@@ -26,22 +27,27 @@ class Cube:
             ]
             for colors, offset in zip(ORIENTATIONS.values(), OFFSETS.values())
         }
-            
+    
+    # assigning to an item using an index
     def __setitem__(self, key, val):
         self.cube_build[JUST_NAMES[key // 9]][key % 9 // 3][key % 3] = val
-        
+    
+    # accessing an item using an index
     def __getitem__(self, key):
         return self.cube_build[JUST_NAMES[key // 9]][key % 9 // 3][key % 3]
-        
+    
+    # produce human readable output
     def __str__(self):
         return ''.join(f'{i[0][0]}{i[0][1]}{i[0][2]}{i[1][0]}{i[1][1]}{i[1][2]}{i[2][0]}{i[2][1]}{i[2][2]}' for i in (self.cube_build[j] for j in ORIENTATIONS.values()))
-        
+    
+    # rotate selection    
     def rotate(self, rotation):
         # upper case - clockwise, lower case - counterclockwise
         direction = rotation.isupper()
         self.rotate_face(rotation, direction)
-        self.rotate_adjacents(rotation, direction)
+        self.rotate_connected(rotation, direction)
         
+    # rotate selected face    
     def rotate_face(self, rotation, direction):
         offset = OFFSETS[rotation.lower()]
         for x in range(0, 1):
@@ -60,9 +66,10 @@ class Cube:
                         self[offset + ((2 - x) * 3 + (2 - y))] = self[offset + ((2 - y) * 3 + x)]
                         self[offset + ((2 - y) * 3 + x)] = z
                     
-    def rotate_adjacents(self, rotation, direction):
-            adjacents = zip(*ADJACENTS[rotation.lower()])
-            for a, b, c, d in adjacents:
+    # rotate blocks connected to selected face                
+    def rotate_connected(self, rotation, direction):
+            connected = zip(*CONNECTED[rotation.lower()])
+            for a, b, c, d in connected:
                 e = self[a]
                 # CLOCKWISE
                 if direction == True:
