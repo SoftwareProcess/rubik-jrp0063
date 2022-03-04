d4cdf1493aba740b3b68359f3ad9f8c221ebd883
Jonathon Porco <jonathon.porco@gmail.com>
2022-03-03 20:41:57 -0600

    Green Light |

--- rubik/test/cubeTest.py
+++ rubik/test/cubeTest.py
@@ -20,70 +20,70 @@ class CubeTest(unittest.TestCase):
         cube.rotate('f')
         self.assertEqual(str(cube), 'gggggggggyrryrryrrbbbbbbbbboowoowoowwwwwwwrrroooyyyyyy')
         
-    def test_cube_04_cuberotate_(self):
+    def test_cube_04_cuberotate_R(self):
         cube_str = 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
         cube = Cube(cube_str)
         cube.rotate('R')
         self.assertEqual(str(cube), 'ggyggyggyrrrrrrrrrwbbwbbwbbooooooooowwgwwgwwgyybyybyyb')
     
     #@unittest.skip    
-    def test_cube_05_cuberotate_(self):
+    def test_cube_05_cuberotate_r(self):
         cube_str = 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
         cube = Cube(cube_str)
         cube.rotate('r')
         self.assertEqual(str(cube), 'ggwggwggwrrrrrrrrrybbybbybbooooooooowwbwwbwwbyygyygyyg')
         
     #@unittest.skip    
-    def test_cube_06_cuberotate_(self):
+    def test_cube_06_cuberotate_B(self):
         cube_str = 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
         cube = Cube(cube_str)
         cube.rotate('B')
         self.assertEqual(str(cube), 'gggggggggrryrryrrybbbbbbbbbwoowoowoorrrwwwwwwyyyyyyooo')
         
-    @unittest.skip    
-    def test_cube_07_cuberotate_(self):
+    #@unittest.skip    
+    def test_cube_07_cuberotate_b(self):
         cube_str = 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
         cube = Cube(cube_str)
         cube.rotate('b')
-        self.assertEqual(str(cube), '')
+        self.assertEqual(str(cube), 'gggggggggrrwrrwrrwbbbbbbbbbyooyooyooooowwwwwwyyyyyyrrr')
         
     @unittest.skip    
-    def test_cube_08_cuberotate_(self):
+    def test_cube_08_cuberotate_L(self):
         cube_str = 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
         cube = Cube(cube_str)
         cube.rotate('L')
         self.assertEqual(str(cube), '')
         
     @unittest.skip    
-    def test_cube_09_cuberotate_(self):
+    def test_cube_09_cuberotate_l(self):
         cube_str = 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
         cube = Cube(cube_str)
         cube.rotate('l')
         self.assertEqual(str(cube), '')
         
     @unittest.skip    
-    def test_cube_10_cuberotate_(self):
+    def test_cube_10_cuberotate_U(self):
         cube_str = 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
         cube = Cube(cube_str)
         cube.rotate('U')
         self.assertEqual(str(cube), '')
         
     @unittest.skip    
-    def test_cube_11_cuberotate_(self):
+    def test_cube_11_cuberotate_u(self):
         cube_str = 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
         cube = Cube(cube_str)
         cube.rotate('u')
         self.assertEqual(str(cube), '')
         
     @unittest.skip    
-    def test_cube_12_cuberotate_(self):
+    def test_cube_12_cuberotate_D(self):
         cube_str = 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
         cube = Cube(cube_str)
         cube.rotate('D')
         self.assertEqual(str(cube), '')
         
     @unittest.skip    
-    def test_cube_13_cuberotate_(self):
+    def test_cube_13_cuberotate_d(self):
         cube_str = 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
         cube = Cube(cube_str)
         cube.rotate('d')
