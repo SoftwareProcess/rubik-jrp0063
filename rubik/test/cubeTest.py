e05a44e6efc6d4387e759e5ce32d015f8923319e
Jonathon Porco <jonathon.porco@gmail.com>
2022-03-03 20:49:31 -0600

    Yellow Light |

--- rubik/test/cubeTest.py
+++ rubik/test/cubeTest.py
@@ -96,6 +96,6 @@ class CubeTest(unittest.TestCase):
     def test_cube_14_rotate_all(self):
         cube_str = 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
         cube = Cube(cube_str)
-        cube.rotate('F')
+        cube.rotate('FfRrBbLlUuDd')
         self.assertEqual(str(cube), 'gggggggggwrrwrrwrrbbbbbbbbbooyooyooywwwwwwooorrryyyyyy')
         
