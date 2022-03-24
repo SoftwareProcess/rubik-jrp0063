86a063e26f2a476da24e1778f023ea01e500e278
Jonathon Porco <jonathon.porco@gmail.com>
2022-03-24 18:44:50 -0500

    Yellow Light |

--- rubik/test/cubeTest.py
+++ rubik/test/cubeTest.py
@@ -127,7 +127,7 @@ class CubeTest(unittest.TestCase):
         test = str(cube)
         test_color = test[49]
         cross = test[46]
-        self.assertEqual('testfail', cross)
+        self.assertEqual(test_color, cross)
         
         
         
