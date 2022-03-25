1160f767eab83964b455914e71914cfb54cd39dc
Jonathon Porco <jonathon.porco@gmail.com>
2022-03-24 21:50:05 -0500

    Green Light |

--- rubik/test/solveTest.py
+++ rubik/test/solveTest.py
@@ -107,4 +107,4 @@ class SolveTest(unittest.TestCase):
         status = result.get('status')
         self.assertEqual(status, 'ok')
         solution = result.get('solution')
-        self.assertEqual(solution, '')
+        self.assertEqual(solution, 'FFURRBBULLfUfUFuRUUuRUUUUUFFUURRUUBBULL')
