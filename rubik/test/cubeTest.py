e1ddc46534a807899a453f99b97f13dfafe432a1
Jonathon Porco <jonathon.porco@gmail.com>
2022-04-10 13:55:38 -0500

    Yellow Light |

--- rubik/test/cubeTest.py
+++ rubik/test/cubeTest.py
@@ -161,7 +161,7 @@ class CubeTest(unittest.TestCase):
         actual = [test[45], test[46], test[47], test[48], test[49], test[50], test[51], test[52], test[53]]
         self.assertEqual(solved, actual)
     
-    #@unittest.skip    
+    @unittest.skip    
     def test_cube_21_downface_solve_from_scrambled(self):
         cube_str = 'ybooroyywyybbgwogoywroorwrwbbrybyrgrwgorygbwggbbowrgwg'
         cube = Cube(cube_str)
@@ -181,7 +181,7 @@ class CubeTest(unittest.TestCase):
         actual = [test[45], test[46], test[47], test[48], test[49], test[50], test[51], test[52], test[53]]
         self.assertEqual(solved, actual)
         
-    @unittest.skip    
+    #@unittest.skip    
     def test_cube_23_downface_solve_from_downcross(self):
         cube_str = 'gyrgrbrrygrbrggrgbwgyooooowgyobbyobworroyywbygwbwwwbwy'
         cube = Cube(cube_str)
