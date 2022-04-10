2dbe9d22bebb8a8a8a4b86d17d2f4e3c46522881
Jonathon Porco <jonathon.porco@gmail.com>
2022-04-10 04:47:16 -0500

    Yellow Light |

--- rubik/test/solveTest.py
+++ rubik/test/solveTest.py
@@ -88,7 +88,7 @@ class SolveTest(unittest.TestCase):
         solution = result.get('solution')
         self.assertEqual(solution, '')
         
-    @unittest.skip
+    #@unittest.skip
     def test_solve_09_bottom_cross_fromdaisy(self):
         parm = {'op': 'solve',
                 'cube': 'xxLxLLxLLEUUUEEUEExLLxxLxxLUEEEUUEUUttttPtPtPPPPPtPtPt'} 
