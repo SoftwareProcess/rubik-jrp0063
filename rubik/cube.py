61603e886e5a76ff8cf94f808d069a1dd52104a1
Jonathon Porco <jonathon.porco@gmail.com>
2022-04-23 20:23:43 -0500

    Green Light |

--- rubik/cube.py
+++ rubik/cube.py
@@ -362,3 +362,5 @@ class Cube:
                 daisy = [self[37], self[39], self[41], self[43]]
                 if daisy == [self[49], self[49], self[49], self[49]]:
                     break
+                else:
+                    self.rotate('U')
